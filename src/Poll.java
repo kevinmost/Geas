805bbdd69490625d2b0aa6c0b54c58621a35b5a3
Kevin Most <kevin.most.93@gmail.com>
2014-01-16 12:19:26 -0500

    Initial public commit

---
+++ src/Poll.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.CookieManager;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.ProxyConfig;
+import com.gargoylesoftware.htmlunit.TextPage;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlRadioButtonInput;
+
+public class Poll {
+	static WebClient client; // Browser
+	static ProxyConfig proxy; // Tor proxy
+	static CookieManager cookies; // Cookies
+	
+	
+	static HtmlPage page; // Webpage for the poll
+	
+	static String pollTitle; // Get the title of the poll
+	static HtmlDivision voteCountDiv; // Text for the choice you selected
+	static HtmlRadioButtonInput choiceButton; // Input for the choice you selected
+
+	public Poll(String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		if (client == null) { // Only do this when the client hasn't been initialized yet
+			client = new WebClient(BrowserVersion.FIREFOX_17); // Other browser versions may not work as smoothly in HtmlUnit
+
+			cookies = new CookieManager(); 
+			client.setCookieManager(cookies);
+
+			proxy = new ProxyConfig("proxy.com", 9150, true); // Place the URL for your TOR proxy here
+			// client.getOptions().setProxyConfig(proxy); // Uncomment this line to use a proxy
+		}
+		page = client.getPage(url); // Get the poll page
+	}
+
+	public void vote(String choice) throws IOException {
+		choiceButton = (HtmlRadioButtonInput) page.getByXPath("//input[contains(@data-side-label, '" + choice + "')]").get(0); // Get the element for the poll option that you want
+		choiceButton.click(); // Click it
+	}
+	public void close() {
+		client.closeAllWindows();
+		cookies.clearCookies();
+	}
+
+	public String getIpAddress() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		return ((TextPage) client.getPage("http://www.icanhazip.com")).getContent().trim(); // Returns your IP address
+	}
+	public String getPollTitle() {
+		return ((HtmlHeading1) page.getElementsByTagName("h1").get(0)).getTextContent(); // Returns this poll's title
+	}
+	public String getVoteCount(String choice) {
+		voteCountDiv = (HtmlDivision) page.getByXPath("//div[contains(@title, '" + choice + "')]").get(0); // Gets the text for the poll option we want
+		String voteCount = voteCountDiv.getElementsByTagName("strong").get(1).getTextContent(); // Gets the 2nd "strong" element, which contains the vote-count
+		int begin = 0;
+		int end = voteCount.indexOf("votes,");
+		voteCount = voteCount.substring(begin, end).replaceAll(",", "").trim(); // Gets text before "votes," and then removes commas and whitespace (which is just the vote-count)
+		return voteCount; // Returns vote count
+	}
+}
---
+++ src/Voter.java
@@ -0,0 +1,61 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+
+public class Voter {
+	
+	static ArrayList<List<String>> pollLists = new ArrayList<List<String>>(); // List containing the polls used. Each element is another List containing two elements (the poll URL and the name of the item we want to vote for)
+	
+	static final String TABLE_FORMAT = "%-11s%-40s%-10s%-20s%-5s\n"; // Table format for when we display the votes that were done
+	
+	static String url; // The variable that holds the URL
+	static String choice; // The variable that holds the choice we are selecting
+	
+	static SimpleDateFormat sdf = new SimpleDateFormat("h:mm:ss a");
+
+	
+	public static void main(String[] args) {
+		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF); // Suppress HtmlUnit's warnings
+		java.util.logging.Logger.getLogger("java.net.SocketException").setLevel(Level.OFF); // Suppress SocketException's warnings
+		java.util.logging.Logger.getLogger("org.apache.http.impl.client.DefaultHttpClient").setLevel(Level.OFF); // Suppress HttpClient's warnings
+		java.util.logging.Logger.getLogger("org.apache.http.NoHttpResponseException").setLevel(Level.OFF); // Suppress HttpClient's warnings
+
+		// Index 0: URL for the poll
+		// Index 1: Name of the poll option you want to select. This name does not have to be exact. The actual poll option only has to contain what you type in as your value, and what you type must be unique in the poll options
+		// EXAMPLE: pollLists.add(Arrays.asList("http://www.poll.com/samplePoll", "sampleChoice"));
+		
+		System.out.format(TABLE_FORMAT, "Date", "Poll Title", "Choice Made", "IP Address", "Vote #");
+		System.out.format(TABLE_FORMAT, "----", "----------", "-----------", "----------", "------");
+
+
+		for(int i = 0; i < Integer.MAX_VALUE; i++) { // You can set how many times the program runs here
+			for (List<String> pollList : pollLists) {
+				try { 
+					// Take the next entry in the iterator and stores its key and value into "url" and "choice", respectively
+					url = pollList.get(0);
+					choice = pollList.get(1);
+
+					Poll poll = new Poll(url); // Create a poll object of this URL and that will click on your specified value
+					poll.vote(choice); // Vote for the previously-specified value
+
+					System.out.format(TABLE_FORMAT, // Prints table of:
+							sdf.format(new Date()), // 40 chars: Formatted time
+							poll.getPollTitle(), // 40 chars: Poll title
+							choice, // 30 chars: Poll option choice
+							poll.getIpAddress(), // 25 chars: IP address used
+							poll.getVoteCount(choice)); // 5 chars: Current option vote count
+
+
+					poll.close(); // Close this page
+				}
+				catch (Exception e) { // If we encounter any errors, just print an error message and keep going
+					System.err.println("Something went wrong with" + url + ". Continuing...");
+				}
+			}
+		}
+
+	}
+}
